b7e32e7f7ff20568c4cb944c85756caf1984ba69
frclasso <frcalsso@yahoo.com.br>
2018-11-16 11:57:27 -0200

    aula 1, cadastroPessoalFisica.py

---
+++ ATIVIDADES/cadastroPessoaFisica.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+
+"""Programa de cadastro de Pessoa Fisica
+
+Utilizem variaveis para salvar valores/dados de uma pessoa, tais como:
+identidade, cpf, nome, sobrenome, telefone, profissao etc"""
+
+
+# exemplo
+
+nome = 'Fabio'
+
+# demais campos ...
+
+#Imprimam os dados
+
+print('Nome:', nome)
+
+
