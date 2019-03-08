b7e962945f85d11829188ca7fa68b617b646ed78
frclasso <frcalsso@yahoo.com.br>
2019-03-08 12:19:15 -0300

    Cap 18 - Extras, final

--- Cap18-Extras/22_writing_csv_bio_stats.py
+++ Cap18-Extras/22_writing_csv_bio_stats.py
@@ -3,12 +3,16 @@
 import csv
 
 with open('bio_stats.csv', 'r') as csv_file:
-    leitor = csv.reader(csv_file)
-    #print(leitor) # end memoria
-    cabecalho = next(leitor)
-    print(cabecalho)
-    print(next(leitor))
-    print(next(leitor))
-    print(next(leitor))
-    for line in leitor:
-        print(line)
+    leitor = csv.DictReader(csv_file)
+
+    with open('bio_copy.csv', 'w') as new_file:
+        fieldnames =['Name', 'Sex', 'Age', 'Weight(lbs)']
+
+        escreve = csv.DictWriter(new_file, fieldnames=fieldnames,
+                                 delimiter=',')
+        escreve.writeheader()
+        for line in leitor:
+            del line['Height(in)']
+            escreve.writerow(line)
+
+print('feito...')
